1175d355ccb07b0cb35bc70cd952504bf47b8ecb
AlonRamirez <alonsormz.mail@gmail.com>
2025-02-05 07:53:19 -0600

    first commit

---
+++ app/src/main/java/com/example/workclass/class/variable.kt
@@ -0,0 +1,39 @@
+fun main(){
+    //Numeric variables
+    val age:Int =20
+    val long_number:Long = 789123128937
+    val temperature:Float = 27.33f
+    val weight:Double = 64.1231
+
+    //String variables
+    val gender:Char = 'M'
+    val name:String = "Ricardo Reyes"
+
+    //Bool variables
+    val isGreater:Boolean = false
+
+    //Collection variables
+    val names = arrayOf("Erick", "Sofia", "Javier", "Veronica")
+
+    println(age)
+    println("Welcome $name, to your first Kotlin project")
+    print(add())
+    println(product(5,8))
+    printArray(names)
+
+
+
+}
+
+fun add():Int{
+    var x = 10
+    var y = 5
+    return(x+y)
+}
+
+fun printArray(names:Array<String>){
+    printlnd(names)
+    for(name in names){
+        println("Hello $name")
+    }
+}
