f0c01d8b3f6ba222918b36166acc7a7cbd025cfe
AlonRamirez <alonsormz.mail@gmail.com>
2025-02-18 20:07:35 -0600

    solucion de errores del archivo variable en la rama develop

--- app/src/main/java/com/example/workclass/class/variable.kt
+++ app/src/main/java/com/example/workclass/class/variable.kt
@@ -1,13 +1,16 @@
+package com.example.workclass
+
 fun main(){
     //Numeric variables
-    val age:Int =20
+    val age:Int = 20
     val long_number:Long = 789123128937
     val temperature:Float = 27.33f
     val weight:Double = 64.1231
 
+
     //String variables
     val gender:Char = 'M'
-    val name:String = "Ricardo Reyes"
+    val name:String = "Alonso Ramirez"
 
     //Bool variables
     val isGreater:Boolean = false
@@ -17,12 +20,11 @@ fun main(){
 
     println(age)
     println("Welcome $name, to your first Kotlin project")
-    print(add())
+    println(add())
     println(product(5,8))
     printArray(names)
-
-
-
+    /*val numbers = ArrayOf(1,2,3,4,5,6,7,8,9,10)
+    isEven(numbers)*/
 }
 
 fun add():Int{
@@ -32,8 +34,22 @@ fun add():Int{
 }
 
 fun printArray(names:Array<String>){
-    printlnd(names)
+    println(names)
     for(name in names){
         println("Hello $name")
     }
+}
+
+fun isEven(numbers:Array<Int>){
+    for(number in numbers){
+        if(number % 2 == 0){
+            println("the number $number is even")
+        }else{
+            println("the number $number is odd")
+        }
+    }
+}
+
+fun product(x:Int,y:Int):Int{
+    return (x*y)
 }
